74e26fd4faa2c54fd8a68bc412093dfc454d1361
mumei001 <mumei001@users.noreply.github.com>
2016-05-24 10:36:15 +0900

    Update HelloWord.c
    20160524 テフン：printf("Helloworld")→printf("Hello, world!")に更新

--- HelloWord.c
+++ HelloWord.c
@@ -6,4 +6,5 @@ int main(void)　                     // こちら２行目
    return 0;                        // こちら５行目 
 
    //１回目修正した
+   // 20160524 テフン：printf("Helloworld")→printf("Hello, world!")に更新
 }                                    // こちら６行目 
