42eff1138bdee00e76686d2e4ae8423b0396184a
mumei001 <mumei001@mailinator.com>
2016-05-27 10:45:09 +0900

    add datatype

--- HelloWord.c
+++ HelloWord.c
@@ -8,7 +8,7 @@ int main(void)　                     // こちら２行目
 　 printf("Thank you for your kindness\n");//７行目
    printf("Oh!! Word!!, you are joking\n"); 
     
-   a=3;  
+   int a=3;  
    // ---------------------
    printf("Oh!Yes, it is my mistake");  //７XXXXXXXXX行目
    printf("I am very sory that ^^");  //７XXXXXXXXX行目
