ad24fb3b767eb404c86424469be43cf877c8b3ef
mayumi <sakaguchi@ok.memail.jp>
2016-05-28 12:54:21 +0900

    I delete some lines

--- HelloWord.c
+++ HelloWord.c
@@ -1,6 +1,6 @@
 #include <stdio.h>                    // こちら１行目 
    
-int main(void)　                     // こちら２行目 
+int main                     // こちら２行目 
 {                                    // こちら３行目 
    printf("Hello, world!");         // こちら４行目 
    printf("この行を追加した");         // こちら４行目 
@@ -8,11 +8,5 @@ int main(void)　                     // こちら２行目
 　 printf("Thank you for your kindness\n");//７行目
    printf("Oh!! Word!!, you are joking\n"); 
     
-   a=3;  
-   // ---------------------
-   printf("Oh!Yes, it is my mistake");  //７XXXXXXXXX行目
-   printf("I am very sory that ^^");  //７XXXXXXXXX行目
-   // ---------------------
-   
    return 0;                        // こちら８行目 
 }                                    // こちら９行目 
